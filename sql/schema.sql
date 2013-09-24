b0301b06220538ab163079810d006d4c86abf620
Amar Mehta <xfthhxk@gmail.com>
2013-09-23 20:14:39 -0400

    using sql store with korma

---
+++ sql/schema.sql
@@ -0,0 +1,61 @@
+drop table if exists job;
+
+create table job ( job_id     int           auto_increment primary key
+                 , job_name   varchar(50)   not null
+                 , job_desc   varchar(255)  not null
+                 , created_at timestamp     not null
+                 , created_by varchar(50)   not null
+                 , updated_at timestamp     not null
+                 , updated_by varchar(50)   not null);
+
+
+drop table if exists job_arg;
+
+create table job_arg ( job_arg_id   int         auto_increment primary key
+                     , job_id       int         not null
+                     , arg_name     varchar(50) not null
+                     , arg_expr     varchar(50) not null
+                     , created_at   timestamp   not null
+                     , created_by   varchar(50) not null
+                     , updated_at   timestamp   not null
+                     , updated_by   varchar(50) not null);
+
+alter table job_arg add constraint fk_job_arg_job_id foreign key(job_id) references job(job_id);
+
+
+drop table if exists job_execution;
+
+create table job_execution ( job_execution_id int       auto_increment primary key
+                           , job_id           int       not null
+                           , started_at       timestamp not null
+                           , finished_at      timestamp null
+                           , success          boolean   null);
+
+alter table job_execution add constraint fk_job_execution_job_id foreign key (job_id) references job(job_id);
+
+
+drop table if exists schedule;
+
+create table schedule( schedule_id     int            auto_increment primary key
+                     , schedule_name   varchar(50)    not null
+                     , schedule_desc   varchar(255)   not null
+                     , cron_expression varchar(50)    not null
+                     , created_at      timestamp      not null
+                     , created_by      varchar(50)    not null
+                     , updated_at      timestamp      not null
+                     , updated_by      varchar(50)    not null);
+
+alter table schedule add constraint unq_schedule_schedule_name unique(schedule_name);
+
+drop table if exists job_schedule;
+
+create table job_schedule ( job_schedule_id int         auto_increment primary key
+                          , job_id          int         not null
+                          , schedule_id     int         not null
+                          , created_at      timestamp   not null
+                          , created_by      varchar(50) not null
+                          , updated_at      timestamp   not null
+                          , updated_by      varchar(50) not null);
+
+alter table job_schedule add constraint fk_job_schedule_job_id foreign key (job_id) references job(job_id);
+alter table job_schedule add constraint fk_job_schedule_schedule_id foreign key (schedule_id) references schedule(schedule_id);
---
+++ src/jsk/db.clj
@@ -0,0 +1,91 @@
+(ns jsk.db
+  "Database access"
+  (:require [clojure.string :as str])
+  (:use [korma core db config]))
+
+; not using any delimiters around table/col name otherwise h2 says table doesn't exist
+
+(def db-url "tcp://localhost:9092/nio:~/projects/jsk/resources/db/jsk.db;AUTO_SERVER=TRUE")
+
+(def db-spec {:classname "org.h2.Driver"
+              :db db-url
+              :subname db-url
+              :user "sa"
+              :subprotocol "h2"
+              :password "" })
+
+
+(defdb jsk-db db-spec)
+
+; used to convert keys to column/field names
+(defn- fields-fn [s]
+  (-> s (str/lower-case) (str/replace "-" "_")))
+
+; used to convert column/field names to keywords
+(defn- keys-fn [s]
+  (-> s (str/lower-case) (str/replace "_" "-")))
+
+; current date time
+(defn- now [] (java.util.Date.))
+
+(set-delimiters "")
+; lower case keywords for result sets
+(set-naming {:keys keys-fn :fields fields-fn})
+
+(declare job job_arg job_execution schedule job_schedule)
+
+
+(defentity schedule
+  (pk :schedule-id)
+  (entity-fields :schedule-id :schedule-name :schedule-desc :cron-expression))
+
+
+(defn ls-schedules
+  []
+  "Lists all schedules"
+  (select schedule))
+
+(defn get-schedule
+  "Gets a schedule for the id specified"
+  [id]
+  (select schedule
+          (where {:schedule-id id})))
+(get-schedule 4)
+
+
+(defn- insert-schedule! [m]
+  (insert schedule
+          (values (merge m {:created-at (:updated-at m)
+                            :created-by (:updated-by m)}))))
+
+(defn- update-schedule! [{:keys [schedule-id] :as m}]
+  (update schedule
+          (set-fields (dissoc m :schedule-id))
+          (where {:schedule-id schedule-id})))
+
+
+(defn save-schedule! [id nm desc cron-expr user-id]
+  (let [m {:schedule-name nm
+           :schedule-desc desc
+           :cron-expression cron-expr
+           :updated-by user-id
+           :updated-at (now)}]
+    (if (neg? id)
+      (insert-schedule! m)
+      (update-schedule! m))))
+
+
+(save-schedule! 9 "My new schedule" "my new schedule desc upd" "anthr cron upd" "amehta-me")
+
+
+
+
+
+
+
+
+
+
+
+
+
