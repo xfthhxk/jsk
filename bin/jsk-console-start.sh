486daecf8aa4707a65bb0f63dc353f8bbef5b7e6
Amar Mehta <xfthhxk@gmail.com>
2014-03-28 21:06:47 +0000

    add start scripts for testing

---
+++ bin/jsk-console-start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+DB_JAR=./lib/h2-1.3.174.jar 
+DB_JAR=./lib/mysql-connector-java-5.1.28-bin.jar 
+
+EXE_JAR=target/jsk-0.1.0-SNAPSHOT-standalone.jar 
+CLASSPATH=${DB_JAR}:${EXE_JAR}
+EXE=jsk.core
+
+java -cp ${CLASSPATH} ${EXE} --mode console --hostname localhost --cmd-port 9000 --status-port 9001 --web-app-port 8080 --nrepl-port 7002 &
